1fcf01d74475ed38de9a5c5047feb0bd2fc9aa8b
dhpyun <dhpyun@dbinc.co.kr>
2024-03-08 17:41:04 +0900

    학습중~!

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
-
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
-  const DefaultLayout({ required this.child, this.backgroundColor,super.key});
+  final String? title;
+  final Widget? bottomNavigationBar;
+
+  const DefaultLayout(
+      {required this.child, this.backgroundColor, this.title, this.bottomNavigationBar, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: renderAppBar(),
       backgroundColor: backgroundColor ?? Colors.white,
       body: child,
+      // 밑에 탭을 생성
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title!,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
