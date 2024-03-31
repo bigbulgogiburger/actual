c4dbce3f558e55f80949b020cca3b89a6f598840
pyeondohun <dohoon321@gmail.com>
2024-03-31 23:02:02 +0900

    Pagination param 작업중

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -3,34 +3,63 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+// base를 미리 만들어줘서(아무것도 없지만), is를 여러번 치기.
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
 @JsonSerializable(
   genericArgumentFactories: true,
 )
-class CursorPagination<T>{
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
-  
-  CursorPagination({
-    required this.meta,
-    required this.data
-});
-  
-  factory CursorPagination.fromJson(Map<String,dynamic> json, T Function(Object? json)fromJsonT)
-  =>_$CursorPaginationFromJson(json,fromJsonT);
 
+  CursorPagination({required this.meta, required this.data});
+
+  factory CursorPagination.fromJson(
+          Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>
+      _$CursorPaginationFromJson(json, fromJsonT);
 }
+
 @JsonSerializable()
-class CursorPaginationMeta{
+class CursorPaginationMeta {
   final int count;
   final bool hasMore;
 
   CursorPaginationMeta({
     required this.count,
     required this.hasMore,
-});
-factory CursorPaginationMeta.fromJson(Map<String,dynamic> json)
-=>_$CursorPaginationMetaFromJson(json);
+  });
 
+  factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
+      _$CursorPaginationMetaFromJson(json);
+}
 
+//새로 고침 할 때에
+// instance is CursorPagination
+// instance is CursorPaginationBase
+// instance is CursorPaginationRefetching
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
 
+// 리스트의 맨 아래로 내려서
+// 추가 데이터를 요청하는 중.
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -13,37 +13,41 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class RestaurantScreen extends ConsumerWidget {
   const RestaurantScreen({super.key});
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final data = ref.watch(restaurantProvider);
 
-    if (data.length == 0) {
+    if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
+
+    final cp = data as CursorPagination;
+
     print('hello');
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: ListView.separated(
-            itemBuilder: (_, index) {
-              final pItem = data[index];
-              return GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) =>
-                          RestaurantDetailScreen(
-                            id: pItem.id,
-                          ),
-                    ));
-                  },
-                  child: RestaurantCard.fromModel(model: pItem));
-            },
-            separatorBuilder: (_, index) {
-              return const SizedBox(height: 16.0);
-            },
-            itemCount: data.length)
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.separated(
+        itemCount: cp.data.length,
+        itemBuilder: (_, index) {
+          final pItem = cp.data[index];
+          return GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => RestaurantDetailScreen(
+                      id: pItem.id,
+                    ),
+                  ),
+                );
+              },
+              child: RestaurantCard.fromModel(model: pItem));
+        },
+        separatorBuilder: (_, index) {
+          return const SizedBox(height: 16.0);
+        },
+      ),
     );
   }
 }
