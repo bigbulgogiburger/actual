7e471c2a5329390a62a0177652bd86a9b05d89ca
dhpyun <dhpyun@dbinc.co.kr>
2024-04-01 17:55:40 +0900

    pagination 완료

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,27 +1,117 @@
 import 'package:actual/common/model/cursor_pagination_model.dart';
+import 'package:actual/common/model/pagination_params.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final restaurantProvider = StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>((ref){
+final restaurantProvider =
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>((ref) {
   final repository = ref.watch(restaurantRepositoryProvider);
   final notifier = RestaurantStateNotifier(repository: repository);
   return notifier;
 });
 
-class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase>{
-
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository,
-}): super(CursorPaginationLoading()){
+  }) : super(CursorPaginationLoading()) {
     paginate();
   }
 
-  paginate() async{
-    final resp = await repository.paginate();
+  void paginate({
+    int fetchCount = 20,
+    // 추가로 데이터를 더 가져오기.
+    // true - 추가로 데이터 더 가져옴
+    // false - 새로고침 (현재 상태를 덮어씌움)
+    bool fetchMore = false,
+    // 강제로 다시 로딩하기.
+    // true - CursorPaginationLoading()
+    bool forceRefetch = false,
+  }) async {
+    try{
+      // 5가지 킹능성
+      // State의 상태
+      // [상태가]
+      // 1) CursorPagination - 정상적으로 데이터가 있는 상태
+      // 2) CursorPaginationloading - 데이터가 로딩중인 상태(현재 캐시가 없음.)
+      // 3) CursorPagination Error - 에러가 있는 상태
+      // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올 때에
+      // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을 때에
 
-    state = resp;
+      // 바로 반환하는 상황
+      // 1) hasmore : false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고 있다면)
+      if (state is CursorPagination && !forceRefetch) {
+        final pState = state as CursorPagination;
+        if (pState.meta.hasMore == false) {
+          return;
+        }
+      }
+      // 2) 로딩중 - fetchMore : true
+      //            fetchMore가 아닐 때에는 함수를 실행한다. 새로 데이터를 가지고 오고 있는데에 새로고침의 의도가 있을 때에
+      final isLoading = state is CursorPaginationLoading;
+      final isRefetching = state is CursorPaginationRefetching;
+      final isFetchingMore = state is CursorPaginationFetchingMore;
+
+      if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
+        return;
+      }
+
+      //PaginationParams 생성
+      PaginationParams paginationParams = PaginationParams(
+        count: fetchCount,
+      );
+
+      // fetchMore
+      // 데이터를 추가로 가져오는 상황.
+      if (fetchMore) {
+        final pState = state as CursorPagination;
+
+        state = CursorPaginationFetchingMore(
+          meta: pState.meta,
+          data: pState.data,
+        );
+
+        paginationParams = paginationParams.copyWith(
+          after: pState.data.last.id,
+        );
+      }
+      // 데이터를 처음부터 가져오는 상황
+      else {
+        // 만약에 데이터가 있는 상황이라면
+        // 기존 데이터를 보존한 채로 Fetch (API 요청)를 진행
+        if (state is CursorPagination && !forceRefetch) {
+          final pState = state as CursorPagination;
+          state = CursorPaginationRefetching(
+            meta: pState.meta,
+            data: pState.data,
+          );
+        } else {
+          state = CursorPaginationLoading();
+        }
+      }
+
+      final resp = await repository.paginate(
+        paginationParams: paginationParams,
+      );
+
+      if (state is CursorPaginationFetchingMore) {
+        final pState = state as CursorPaginationFetchingMore;
+
+        // 기존 데이터에
+        // 새로운 데이터 추가.
+        state = resp.copyWith(
+          data: [
+            ...pState.data,
+            ...resp.data,
+          ],
+        );
+      }else{
+        state = resp;
+      }
+    }catch(e){
+      state = CursorPaginationError(message: '데이터를 가져오지 못했습니다.');
+    }
   }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -10,27 +10,74 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scrollController.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가
+    // 최대 길이보다 조금 덜 되는 위치까지 왔다면
+    // 새로운 데이터를 추가 요청.
+    if (scrollController.offset >
+        scrollController.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    // 완전 처음 로딩일 떄에
     if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
 
+    // 에러
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // CursorPagination 및 그의 자식들
+    // CursorPaginationFetchingmore
+    // CursorPaginationRefetching
+
     final cp = data as CursorPagination;
 
     print('hello');
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: scrollController,
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: data is CursorPaginationFetchingMore? CircularProgressIndicator() : Text('마지막 테이터 입니다 ㅠㅠ'),
+              ),
+            );
+          }
           final pItem = cp.data[index];
           return GestureDetector(
               onTap: () {
